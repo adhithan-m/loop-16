8beedbbc3372cae07f47492a12c80ccee1103a4b
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 19:53:02 +0530

    duplicate.

---
+++ duplicate/js/duplicate.js
@@ -0,0 +1,6 @@
+//#duplicate identifier 
+// var venkat = prompt("Enter the string"); //adhi is and adhi is
+// var temp = (venkat.match(/is/g))
+// var temp1 = (venkat.match(/is/i)) //is,is
+// var length = (temp.length);
+// document.write("The word "+temp1+" has founded "+length+" times")
